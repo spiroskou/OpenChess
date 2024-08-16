44d3ea6cba504be43321d8f4a95ec24d21c433d6
spiroskou <skoulouris77@gmail.com>
2024-08-16 21:22:19 +0300

    updates on cmake

--- cmake/modules/FindSDL2.cmake
+++ cmake/modules/FindSDL2.cmake
@@ -1,12 +1,7 @@
-# FindSDL2.cmake
-# Attempt to find the SDL2 library
-
-# Look for the header file.
 find_path(SDL2_INCLUDE_DIR NAME SDL.h
           HINTS $ENV{SDL2DIR}
           PATH_SUFFIXES SDL2 include/SDL2 include)
 
-# Look for the library.
 find_library(SDL2_LIBRARY NAME SDL2
              HINTS $ENV{SDL2DIR}
              PATH_SUFFIXES lib64 lib32 lib
@@ -16,10 +11,16 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SDL2 REQUIRED_VARS SDL2_LIBRARY SDL2_INCLUDE_DIR)
 
 if(SDL2_FOUND AND NOT TARGET SDL2::SDL2)
-    add_library(SDL2::SDL2 UNKNOWN IMPORTED)
+    add_library(SDL2::SDL2 SHARED IMPORTED)
     set_target_properties(SDL2::SDL2 PROPERTIES
         IMPORTED_LOCATION "${SDL2_LIBRARY}"
         INTERFACE_INCLUDE_DIRECTORIES "${SDL2_INCLUDE_DIR}")
+endif()
+
+if(EXISTS "${SDL2_LIBRARY}" AND EXISTS "${SDL2_INCLUDE_DIR}")
+    set(SDL2_SDL2_FOUND TRUE)
+else()
+    set(SDL2_SDL2_FOUND FALSE)
 endif()
 
 mark_as_advanced(SDL2_INCLUDE_DIR SDL2_LIBRARY)
