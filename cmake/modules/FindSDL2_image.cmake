5e92083f589f5185b9cc67ea2950207cfbc13936
spiroskou <skoulouris77@gmail.com>
2024-08-16 20:06:36 +0300

    update CMakeLists file

---
+++ cmake/modules/FindSDL2_image.cmake
@@ -0,0 +1,13 @@
+find_path(SDL2_IMAGE_INCLUDE_DIR SDL_image.h)
+find_library(SDL2_IMAGE_LIBRARY NAMES SDL2_image)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(SDL2_image DEFAULT_MSG SDL2_IMAGE_LIBRARY SDL2_IMAGE_INCLUDE_DIR)
+
+if(SDL2_IMAGE_FOUND)
+  set(SDL2_IMAGE_LIBRARIES ${SDL2_IMAGE_LIBRARY})
+  set(SDL2_IMAGE_INCLUDE_DIRS ${SDL2_IMAGE_INCLUDE_DIR})
+endif()
+
+mark_as_advanced(SDL2_IMAGE_INCLUDE_DIR SDL2_IMAGE_LIBRARY)
+
