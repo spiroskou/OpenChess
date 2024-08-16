917f21cf72a61e035fdd1c77a43572c23657093d
spiroskou <skoulouris77@gmail.com>
2024-08-16 21:11:25 +0300

    update

--- cmake/modules/FindSDL2.cmake
+++ cmake/modules/FindSDL2.cmake
@@ -1,10 +1,19 @@
-# Locate SDL2 library and include directories
-find_path(SDL2_INCLUDE_DIR NAME SDL.h HINTS ENV SDL2DIR PATH_SUFFIXES include/SDL2 include)
+# FindSDL2.cmake
+# Attempt to find the SDL2 library
 
-find_library(SDL2_LIBRARY NAME SDL2 HINTS ENV SDL2DIR PATH_SUFFIXES lib lib/x64 lib/x86)
+# Look for the header file.
+find_path(SDL2_INCLUDE_DIR NAME SDL.h
+          HINTS $ENV{SDL2DIR}
+          PATH_SUFFIXES SDL2 include/SDL2 include)
+
+# Look for the library.
+find_library(SDL2_LIBRARY NAME SDL2
+             HINTS $ENV{SDL2DIR}
+             PATH_SUFFIXES lib64 lib32 lib
+             PATHS ${SDL2_PATH})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SDL2 DEFAULT_MSG SDL2_LIBRARY SDL2_INCLUDE_DIR)
+find_package_handle_standard_args(SDL2 REQUIRED_VARS SDL2_LIBRARY SDL2_INCLUDE_DIR)
 
 if(SDL2_FOUND AND NOT TARGET SDL2::SDL2)
     add_library(SDL2::SDL2 UNKNOWN IMPORTED)
@@ -14,4 +23,3 @@ if(SDL2_FOUND AND NOT TARGET SDL2::SDL2)
 endif()
 
 mark_as_advanced(SDL2_INCLUDE_DIR SDL2_LIBRARY)
-
